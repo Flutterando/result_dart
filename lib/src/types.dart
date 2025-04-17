797e93ce32fb32656e0336c0812d2b9ac591692e
Heng <leanghenglim@hotmail.com>
2025-04-18 07:50:32 +1000

    Changed default type to Object to support all Object types as well otherwise catching an error will always need an explicit on Exception clause, which is rather annoying.

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -1,18 +1,18 @@
 import 'package:result_dart/result_dart.dart';
 
 /// A typedef for a `Result` that simplifies the usage of `ResultDart`
-/// with `Exception` as the default failure type.
+/// with `Object` as the default failure type.
 ///
 /// This is used to represent operations that can succeed with a
 /// value of type `S`
-/// or fail with an `Exception`.
-typedef Result<S> = ResultDart<S, Exception>;
+/// or fail with an `Object`.
+typedef Result<S> = ResultDart<S, Object>;
 
 /// A typedef for an asynchronous `Result`, simplifying the usage
 /// of `AsyncResultDart`
-/// with `Exception` as the default failure type.
+/// with `Object` as the default failure type.
 ///
 /// This is used to represent asynchronous operations that can succeed
 /// with a value of type `S`
-/// or fail with an `Exception`.
-typedef AsyncResult<S> = AsyncResultDart<S, Exception>;
+/// or fail with an `Object`.
+typedef AsyncResult<S> = AsyncResultDart<S, Object>;
